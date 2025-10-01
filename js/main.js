dc35c32f04352139759cb637c4599f97f95f16ba
soty <websoty@gmail.com>
2025-10-01 13:29:23 +0300

    Добавлен простой минимальный слайдер, добавил плавность в стилях при прокрутке

--- js/main.js
+++ js/main.js
@@ -3,9 +3,39 @@ const showMenu = document.querySelector('.nav__show')
 const closedMenu = document.querySelector('.nav__head-btn.menu')
 
 openMenu.addEventListener('click', () => {
-  showMenu.classList.toggle('active');
+  showMenu.classList.add('active');
 });
 
 closedMenu.addEventListener('click', () => {
   showMenu.classList.remove('active');
 })
+
+
+// slider
+
+const track = document.querySelector('.slider__track');
+const [btnPrev, btnNext] = document.querySelectorAll('.slider__btns button');
+const slides = Array.from(track.children); // массив слайдов
+
+let currentSlide = 0;
+
+  btnPrev.addEventListener('click', () => {
+    currentSlide--;
+  let slideWidth = slides[0].offsetWidth;
+  if (currentSlide < 0) 
+    currentSlide = slides.length - 1;
+  let offset = slideWidth * currentSlide;
+
+  track.style.transform = `translateX(-${offset}px)`
+  });
+
+   btnNext.addEventListener('click', () => {
+    currentSlide++;
+  let slideWidth = slides[0].offsetWidth;
+  if (currentSlide > slides.length -1 )
+    currentSlide = 0;
+
+  let offset = slideWidth * currentSlide;
+
+  track.style.transform = `translateX(-${offset}px)`
+  });
