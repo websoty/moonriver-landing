7e52e8c8f67bc3f8e9f9b5a0036294599bc190d2
soty <websoty@gmail.com>
2025-10-01 14:49:27 +0300

    feat: валидация емаил и модальное окно, style: стили для ошибки и модального окна, fix: в html вставил атрибут novalidate

--- js/main.js
+++ js/main.js
@@ -39,3 +39,52 @@ let currentSlide = 0;
 
   track.style.transform = `translateX(-${offset}px)`
   });
+
+
+// form 
+
+const forms = document.querySelectorAll('form');
+
+  forms.forEach((form) => {
+    const inputEmail = form.querySelector('input[type="email"]');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+    const valueEmail = inputEmail.value.trim();
+
+      if (valueEmail && valueEmail.includes('@')) {
+        showModal('Спасибо, Ваш e-mail отправлен!');
+      } else {
+        const msg = document.createElement('div');
+        msg.textContent = "Ошибка: неккоректно указан e-mail! Адрес электронной почты должен содержать символ '@'";
+        msg.classList.add('error');
+        const haveMsg = form.querySelector('.error');
+        if (haveMsg) {
+          haveMsg.remove();
+        }
+        inputEmail.after(msg);
+      }
+    });
+      inputEmail.addEventListener('input', () => {
+      const haveMsg = form.querySelector('.error');
+      if (haveMsg) haveMsg.remove();
+    });
+  });
+
+  const showModal = (message) => {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.innerHTML = `
+    <div class="modal-content">
+      <p>${message}</p>
+      <button class="modal-close">Закрыть</button>
+    </div>`;
+
+    document.body.appendChild(modal);
+    modal.querySelector('.modal-close').addEventListener('click', () => {
+      modal.remove();
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) modal.remove();
+    });
+  };
